e34f69bbb90d1e681d06d7cc02decbab1e8038d5
vortex7 <sean.wright.vortex@gmail.com>
2020-04-03 14:21:26 -0700

    mods

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "devops",
-  PASSWORD: "Zulu7!!",
-  DB: "mydb",
+  USER: "test",
+  PASSWORD: "test",
+  DB: "testdb",
   PORT: 1433,
   dialect: "mysql",
   pool: {
